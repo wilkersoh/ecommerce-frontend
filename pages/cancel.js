6f3c597237095a18fa5b3c86480c842294ffe2ea
wilkersoh <plgu@hotmail.com>
2021-02-19 23:36:07 +0800

    Update Cancel order UI

--- pages/cancel.js
+++ pages/cancel.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
+import Image from "next/image";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
 import { API_URL } from "../utils/urls";
+import App from "../components/App";
 import { useCart } from "../context/CartContext";
 
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Heading, Link } from "@chakra-ui/react";
+import { twoDecimals } from "../utils/format";
 
 const queryKey = "cancel_id";
 
@@ -49,28 +52,77 @@ export default function cancel() {
 
   const { loading } = useOrderCancelation(cancel_id);
 
-  if (loading)
+  if (loading || !cartItems)
     return (
-      <>
+      <App>
         <Head>We are cancelling your payment</Head>
-        <p>Wating a momnet... we are handling your payment.</p>
-      </>
+        <Text>Wating a momnet... we are handling your payment.</Text>
+      </App>
     );
 
+  console.log("cartItems :>> ", cartItems);
+
   return (
-    <Box>
+    <App>
       <Head>
         <title>Successful cancelation</title>
       </Head>
-      <h2>Cancel!</h2>
-      <NextLink href='/cart'>
-        <a>Backkkk</a>
-      </NextLink>
 
-      <Text>Your just cancelling:</Text>
+      <Heading as='h1' mb={6}>
+        Your just cancelling:
+      </Heading>
+
       {cartItems.map((cart) => (
-        <Box>{cart.product.name}</Box>
+        <Box key={cart.id} d='flex' mb={4}>
+          <Box w='20%' mr={6}>
+            <NextLink
+              href={
+                cart.category_slug
+                  ? `/categories/${cart.category_slug}/product/${cart.product.product_slug}`
+                  : `/products/${cart.product.product_slug}`
+              }
+              passHref>
+              <Link>
+                <Image
+                  src={cart.product.images[0].url}
+                  width={200}
+                  height={200}
+                  layout='responsive'
+                />
+              </Link>
+            </NextLink>
+          </Box>
+          <Box w='80%'>
+            <Heading as='h2' fontSize='1.29em' color='green.1'>
+              <NextLink
+                href={
+                  cart.category_slug
+                    ? `/categories/${cart.category_slug}/product/${cart.product.product_slug}`
+                    : `/products/${cart.product.product_slug}`
+                }
+                passHref>
+                <Link _hover={{ textDecor: "underline" }}>
+                  {cart.product.name}
+                </Link>
+              </NextLink>
+            </Heading>
+            <Box fontSize='17px'>
+              <Text as='h3' mt={3} fontWeight='400'>
+                RM {twoDecimals(cart.product.price)} / each
+              </Text>
+              <Text mt={3} as='h3'>
+                Quantity - {cart.quantity}
+              </Text>
+              <Text mt={3}>
+                Total:{" "}
+                <Text as='span' fontWeight='600' color='green.1'>
+                  RM {twoDecimals(cart.product.price * cart.quantity)}
+                </Text>
+              </Text>
+            </Box>
+          </Box>
+        </Box>
       ))}
-    </Box>
+    </App>
   );
 }
