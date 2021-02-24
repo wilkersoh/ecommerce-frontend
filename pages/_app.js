f01385f5360cfd6dae937172b2d79a4399b71f5c
wilkersoh <plgu@hotmail.com>
2021-02-24 15:06:00 +0800

    Update banner code

--- pages/_app.js
+++ pages/_app.js
@@ -23,22 +23,22 @@ function MyApp({ Component, pageProps }) {
             <CartProvider>
               <DefaultSeo
                 {...SEO}
-                openGraph={{
-                  type: "website",
-                  locale: "en_IE",
-                  url: "https://creative.staging.selfpaths.com/",
-                  site_name: "Creative 文具屋",
-                  images: [
-                    {
-                      url:
-                        "https://creative.staging.selfpaths.com/images/facebook_1.jpg",
-                      width: 1200,
-                      height: 630,
-                      alt:
-                        "Creative 文具 | Find the best stationery deals and happy hours in your area.",
-                    },
-                  ],
-                }}
+                // openGraph={{
+                //   type: "website",
+                //   locale: "en_IE",
+                //   url: "https://creative.staging.selfpaths.com/",
+                //   site_name: "Creative 文具屋",
+                //   images: [
+                //     {
+                //       url:
+                //         "https://creative.staging.selfpaths.com/images/facebook_1.jpg",
+                //       width: 1200,
+                //       height: 630,
+                //       alt:
+                //         "Creative 文具 | Find the best stationery deals and happy hours in your area.",
+                //     },
+                //   ],
+                // }}
               />
               <Component {...pageProps} />
             </CartProvider>
